a1895a68833c6445aa35eb2813e706d74f9d131c
Sameer Ahmed S <sameer.contrib@gmail.com>
2025-04-29 16:33:16 +0530

    after upgrading angular/core and angular/cli from v17 to v18 without new build system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AlbumsComponent } from './albums/albums.component';
@@ -10,20 +10,17 @@ import { NgxsResetPluginModule } from 'ngxs-reset-plugin';
 import { AlbumsState } from './store/albums/albums.state';
 import { PhotosState } from './store/photos/photos.state';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AlbumsComponent,
-    PhotosComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule, // Import HttpClientModule to make HTTP requests
-    NgxsModule.forRoot([AlbumsState,PhotosState]), // Initialize NGXS store with an empty state
-    NgxsResetPluginModule.forRoot() ,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({    
+     declarations: [
+        AppComponent,
+        AlbumsComponent,
+        PhotosComponent,
+    ],
+    bootstrap: [AppComponent],
+     imports: [BrowserModule, // Import HttpClientModule to make HTTP requests
+        NgxsModule.forRoot([AlbumsState, PhotosState]), // Initialize NGXS store with an empty state
+        NgxsResetPluginModule.forRoot(),
+        AppRoutingModule], 
+        providers: [provideHttpClient(withInterceptorsFromDi())] 
+    })
 export class AppModule {}
